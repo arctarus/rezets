f247d3049824d13a36714a59366d736fcdf8c6b8
Gabriel Ortuño <gaby@mastropiero.(none)>
2010-03-15 23:13:14 +0100

    migrate recipes photo name to recipe slug

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091107181752) do
+ActiveRecord::Schema.define(:version => 20100312182635) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
