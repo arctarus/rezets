67ac9b5411b93fe193ffbd1a7b4779deee315ded
Gabriel Ortuño <gaby@mastropiero.(none)>
2010-04-03 13:09:37 +0200

    no enviar email a autor del comentario actual

--- app/models/comment_observer.rb
+++ app/models/comment_observer.rb
@@ -5,7 +5,7 @@ class CommentObserver < ActiveRecord::Observer
     recipe = comment.recipe
     users = recipe.comments.collect {|c| c.user}.uniq
     users << recipe.author unless users.include? recipe.author
-#   users.delete(current_user)
+    users.delete(current_user)
     users.each do |user|
       UserMailer.deliver_comment_recipe(comment, user)
     end
