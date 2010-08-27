ac046eddfb2c25cf308464131f60c222d178edde
Gabriel Ortuño <gaby@mastropiero.(none)>
2010-08-27 18:54:42 +0200

    enviar por email twitter imprimir

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,10 +1,11 @@
 class UserMailer < ActionMailer::Base
+  helper :recipes
  
  def feedback(feedback)
    recipients "rezets.com@gmail.com"
    from       feedback['email']
    reply_to   feedback["email"]
-   subject    "feedback de #{feedback["name"]}"
+   subject    _("feedback from #{feedback["name"]}")
    sent_on    Time.now
    body       :name => feedback["name"], :message => feedback["message"]
  end 
@@ -21,13 +22,13 @@ class UserMailer < ActionMailer::Base
  def comment_recipe(comment, user)
    # el autor recibe el mail de un comentario de otro
    if user == comment.recipe.author
-     _subject = "#{comment.user.name} ha comentado tu receta de #{comment.recipe.name}"
+     _subject = _("%{user} has commented on your %{recipe} recipe") % {:user => comment.user.name, :recipe => comment.recipe.name.downcase}
    # el que comenta la receta es el autor pero se envia a otro
    elsif comment.user == comment.recipe.author
-     _subject = "#{comment.user.name} ha comentado su receta de #{comment.recipe.name}"
+     _subject = _("%{user} has commented on his %{recipe} recipe") % {:user => comment.user.name, :recipe => comment.recipe.name.downcase}
    # un usuario comenta la receta de otro y le llega a un 3º
    else
-    _subject = "#{comment.user.name} ha comentado la receta de #{comment.recipe.name} de #{comment.recipe.author.name}"
+    _subject = _("%{user} has commented on %{author}'s %{recipe} recipe") % {:user => comment.user.name, :recipe => comment.recipe.name.downcase, :author => comment.recipe.author.name}
   end
 
    recipients user.email
@@ -36,6 +37,16 @@ class UserMailer < ActionMailer::Base
    subject    _subject
    sent_on    Time.now
    body       :subject => _subject, :user => user, :comment => comment
+ end
+
+ def recipe(recipe, email, current_user)
+   user_name = email[:name].blank? ? current_user.name : email[:name]
+   recipients email['recipients'].split(';')
+   from       "rezets.com@gmail.com"
+   reply_to   "rezets.com@gmail.com"
+   subject    _("%{user} has sent you a %{recipe} recipe") % {:user => user_name, :recipe => recipe.name.downcase}
+   sent_on    Time.now
+   body       :recipe => recipe, :user_name => current_user.nil? ? user_name : current_user.name, :message => email['message']
  end
 
 end
