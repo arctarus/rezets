343e1fb4f9501fab1fb2f59e1193c2797f6ac964
Gabriel Ortuño <arctarusnet@gmail.com>
2010-11-22 23:01:09 +0100

    rails best practices

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,9 +2,9 @@ class CategoriesController < ApplicationController
   layout "base"
 
   def index
-    @recipes = Recipe.all(:order => "created_at desc").paginate(:page => params[:page], :per_page => 10)
-    @categories = Category.all(:order => "name asc")
-    @categories.delete_if {|c| c.recipes.size <= 0 }
+    @recipes = Recipe.order("created_at desc").paginate(:page => params[:page])
+    @categories = Category.with_recipes.order("name asc")
+
     @page_title = "recetas por categorias"
     if not params[:page].nil? and params[:page].to_i > 1
       @page_title << " pagina #{params[:page]}"
@@ -17,11 +17,12 @@ class CategoriesController < ApplicationController
   # GET /categories/1.xml
   def show
     @category = Category.find_by_slug params[:id]
-    @recipes = Recipe.all(:conditions => {
-      :category_id => @category.id },
-      :order => "created_at desc").paginate(:page => params[:page], :per_page => 10)
-    @categories = Category.all(:order => "name asc")
-    @categories.delete_if {|c| c.recipes.size <= 0 }
+    @recipes = Recipe.where(:category_id => @category.id).
+      order("created_at desc").
+      paginate(:page => params[:page])
+
+    @categories = Category.with_recipes.order("name asc")
+
     @page_title = "recetas de #{@category.name}"
     @page_identifier = "categories"
     respond_to do |format|
