533f096fdc53a3fe771537f4389dd38ae70208c8
Gabriel Ortuño <arctarusnet@gmail.com>
2010-12-18 19:25:04 +0100

    change font-size

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def title(title)
+    content_for :title, title
+  end
+
+  def page_title
+    page_title = if content_for? :title
+      content_for(:title) + ' - '
+    end
+    page_title += 'rezets.com'
+  end
+
 end
