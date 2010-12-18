346d90239c2270d29ad065885fa172c15eca574d
Gabriel Ortuño <arctarusnet@gmail.com>
2010-12-18 19:25:04 +0100

    change font-size

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,35 +1,31 @@
 class HomeController < ApplicationController
-  layout 'base'
 
   def index
     @recipes = Recipe.order("created_at desc").limit(8)
     @categories = Category.with_recipes.order("name asc")
     @users = User.featured
     @page_identifier = "home"
-    @page_title = _("share your recipes")
   end
 
   def feed
     @recipes = Recipe.all(:order => "created_at desc", :limit => 5)
     respond_to do |format|
-      format.rss { render :layout     => false, 
-                          :action     => "feed" }
+      format.rss { render :layout => false, 
+                          :action => "feed" }
     end
   end
 
   def about
     @page_class = "single"
-    @page_title = _("about rezets.com")
   end
 
   def feedback
-    @page_title = _("contact with us")
   end
 
   def send_feedback
     UserMailer.feedback(params[:feedback]).deliver
     flash[:notice] = _("thanks for your help")
-    redirect_to :action => "index"
+    redirect_to root_url
   end
   
   def search
