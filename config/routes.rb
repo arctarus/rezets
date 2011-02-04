f8afd9b3afc410a030518bbcb49cc0378fed5e12
Gabriel Ortuño <arctarusnet@gmail.com>
2011-02-04 20:33:36 +0100

    more rededign

--- config/routes.rb
+++ config/routes.rb
@@ -1,51 +1,5 @@
 Rezets::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
+  
   resources :recipes do
     member do
       post :save
@@ -53,7 +7,6 @@ Rezets::Application.routes.draw do
     end
     resources :comments
   end
-  #:has_many => [ :comments ]
   
   resources :users do
     member do
@@ -62,7 +15,6 @@ Rezets::Application.routes.draw do
     end
     resources :recipes
   end
-  #:has_many => [ :recipes ]
 
   resources :users do
     resources :recipes do
