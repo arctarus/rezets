4d9bf3ec562981855e34c700b40578bea081df64
Gabriel Ortuño <arctarusnet@gmail.com>
2011-02-19 12:00:53 +0100

    follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,25 @@ class User < ActiveRecord::Base
   has_many :invitations_sent, :foreign_key => "sender_id", :class_name => "Invitation"
   has_many :invitations_received, :foreign_key => "receiver_id", :class_name => "Invitation"
 
+  has_and_belongs_to_many :followers, 
+    :join_table => 'follows', 
+    :class_name => 'User',
+    :association_foreign_key => 'follower_id',
+    :foreign_key => 'following_id'
+
+  has_and_belongs_to_many :followings,
+    :join_table => 'follows', 
+    :class_name => 'User',
+    :association_foreign_key => 'following_id',
+    :foreign_key => 'follower_id'
+
   validates_presence_of :email, :slug, :name
   validates_uniqueness_of :slug
 
   has_attached_file :avatar, :styles => {
-    :thumb  => "20>x20",
-    :medium => "30>x30",
-    :large  => "80>x80" }
+    :thumb  => "20x20#",
+    :medium => "30x30#",
+    :large  => "80x80#" }
 
   scope :featured,
     select('users.*, count(user_recipes.id) num_recipes').
