6d8db2b205696fde6784866781863d0659da03b5
Gabriel Ortuño <arctarusnet@gmail.com>
2011-03-06 11:36:05 +0100

    counter caches

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,9 @@
 class Follow < ActiveRecord::Base
-  belongs_to :follower, :class_name => 'User', :foreign_key => :following_id, :counter_cache => :followers_count
-  belongs_to :following, :class_name => 'User', :foreign_key => :follower_id, :counter_cache => :followings_count
+  belongs_to :follower, :class_name => 'User', 
+                        :foreign_key => :following_id, 
+                        :counter_cache => :followers_count
+
+  belongs_to :following, :class_name => 'User', 
+                         :foreign_key => :follower_id, 
+                         :counter_cache => :followings_count
 end
