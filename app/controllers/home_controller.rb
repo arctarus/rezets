6c3dbb57855fc56ced805c21ee406e2c194f71f1
Gabriel Ortuño <arctarusnet@gmail.com>
2011-03-06 13:04:56 +0100

    home styles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,22 +1,17 @@
 class HomeController < ApplicationController
+  respond_to :html, :except => :feed
 
   def index
     @recipes = Recipe.order("created_at desc").limit(8)
     @categories = Category.with_recipes.order("name asc")
     @users = User.featured.limit(8)
-    @page_identifier = "home"
   end
 
   def feed
-    @recipes = Recipe.all(:order => "created_at desc", :limit => 5)
-    respond_to do |format|
-      format.rss { render :layout => false, 
-                          :action => "feed" }
-    end
+    @recipes = Recipe.order("created_at desc").limit(8)
   end
 
   def about
-    @page_class = "single"
   end
 
   def feedback
