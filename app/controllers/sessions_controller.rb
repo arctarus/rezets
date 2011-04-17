4d006f34f40fbee59e5ec91d4c9193f29b78680c
Gabriel Ortuño <arctarusnet@gmail.com>
2011-04-17 19:14:18 +0200

    and more work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,22 +4,19 @@ class SessionsController < ApplicationController
   
   def new
     @user_session = UserSession.new
-    @page_title = "entrar en rezets.com"
   end
   
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
-      flash[:notice] = _("welcome!")
       redirect_back_or_default @user_session.user
     else
-      render :action => "new"
+      render "new"
     end
   end
   
   def destroy
     current_user_session.destroy
-    flash[:notice] = _("see you soon!")
     redirect_back_or_default "home#index"
   end
 
