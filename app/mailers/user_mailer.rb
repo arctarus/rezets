7c44b31f06ab295ba8e21074b6d61cad0c562e19
Gabriel Ortuño <arctarusnet@gmail.com>
2011-04-19 16:01:04 +0200

    fix comment recipe mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -21,21 +21,21 @@ class UserMailer < ActionMailer::Base
   end
 
   def comment_recipe(comment, user)
-    # el autor recibe el mail de un comentario de otro
-    if user == comment.recipe.author
-      _subject = _("%{user} has commented on your %{recipe} recipe") % {:user => comment.user.name, :recipe => comment.recipe.name.downcase}
-      # el que comenta la receta es el autor pero se envia a otro
-    elsif comment.user == comment.recipe.author
-      _subject = _("%{user} has commented on his %{recipe} recipe") % {:user => comment.user.name, :recipe => comment.recipe.name.downcase}
-      # un usuario comenta la receta de otro y le llega a un 3º
-    else
-      _subject = _("%{user} has commented on %{author}'s %{recipe} recipe") % {:user => comment.user.name, :recipe => comment.recipe.name.downcase, :author => comment.recipe.author.name}
-    end
     @user = user
     @comment = comment
-    @subject = _subject
+    # el autor recibe el mail de un comentario de otro
+    if @user == @comment.recipe.author
+      @title = _("%{user} has commented on your %{recipe} recipe") % {:user => @comment.user.name, :recipe => @comment.recipe.name.downcase}
+      # el que comenta la receta es el autor pero se envia a otro
+    elsif @comment.user == @comment.recipe.author
+      @title = _("%{user} has commented on his %{recipe} recipe") % {:user => @comment.user.name, :recipe => @comment.recipe.name.downcase}
+      # un usuario comenta la receta de otro y le llega a un 3º
+    else
+      @title = _("%{user} has commented on %{author}'s %{recipe} recipe") % {:user => @comment.user.name,
+        :recipe => @comment.recipe.name.downcase, :author => comment.recipe.author.name}
+    end
     mail :to => @user.email,
-      :subject => @subject
+      :subject => @title
   end
 
   def recipe(recipe, email, current_user)
