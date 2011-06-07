614ccfac3405a1012b1c9315a12c4bd43d53f75a
Gabriel Ortuño <arctarusnet@gmail.com>
2011-06-07 20:05:35 +0200

    twitter gem and finalizing about styling

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = "KWLj32kssD0FVFNyHH7JFg"
+  config.consumer_secret = "qGnKN14dDkdKshFZoJIRT3vOWiklouo1cJQpeftW0"
+  config.oauth_token = "7822002-NIknvD6VLdqDce55NvgDvT823ZGP20o5uxfptJW1w"
+  config.oauth_token_secret = "179O4ko3trcgpPeNLn1JL3cZRw68LcoheM28bMyShY"
+end
