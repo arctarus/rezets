e339d5b76436e20314ca77384c021bbb4d30f85a
Gabriel Ortuño <arctarusnet@gmail.com>
2011-06-12 00:33:41 +0200

    new recipe acceptance spec

---
+++ spec/factories/user.rb
@@ -0,0 +1,7 @@
+Factory.define :user do |user|
+  user.name { Faker::Name.name }
+  user.email { Faker::Internet.email }
+  user.password "123456"
+  user.password_confirmation "123456"
+  user.slug { Faker::Name.name.parameterize }
+end
