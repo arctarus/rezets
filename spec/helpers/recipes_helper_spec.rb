e875371ab2af7f5641ed2a56f769e072e2e9ada7
Gabriel Ortuño <arctarusnet@gmail.com>
2011-06-12 12:50:28 +0200

    show rounded number on ingredient values

--- spec/helpers/recipes_helper_spec.rb
+++ spec/helpers/recipes_helper_spec.rb
@@ -1,11 +1,31 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe RecipesHelper do
-  
-  #Delete this example and add some real ones or delete this file
-  it "should be included in the object returned by #helper" do
-    included_modules = (class << helper; self; end).send :included_modules
-    included_modules.should include(RecipesHelper)
+ 
+  describe "round" do
+    it "0.5 return 1/2" do
+      round(0.5).should == "&frac12;"
+    end
+
+    it "0.25 return 1/4" do
+      round(0.25).should == "&frac14;"
+    end
+
+    it "0.3 return 1/3" do
+      round(0.3).should == "&#8531;"
+    end
+
+    it "0.2 return 1/5" do
+      round(0.2).should == "&#8533;"
+    end
+
+    it "2.0 return 2" do
+      round(2.0).should == 2
+    end
+
+    it "2.5 return 2.5" do
+      round(2.5).should == 2.5
+    end
   end
-  
+
 end
