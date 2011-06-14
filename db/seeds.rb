230d3f8359636aa5b5b80484a4e659f26cb215ba
Gabriel Ortuño <arctarusnet@gmail.com>
2011-06-14 20:59:03 +0200

    add categories to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+categories = [
+  { :name => "verduras y hortalizas", :slug => "verduras-y-hortalizas" },
+  { :name => "carne",                 :slug => "carne" }, 
+  { :name => "pescados y mariscos",   :slug => "pescados-y-mariscos" }, 
+  { :name => "aves y caza",           :slug => "aves-y-caza" }, 
+  { :name => "legumbres",             :slug => "legumbres" }, 
+  { :name => "pasta",                 :slug => "pasta" }, 
+  { :name => "arroz",                 :slug => "arroz" }, 
+  { :name => "postres",               :slug => "postres" }, 
+  { :name => "sopas y caldos",        :slug => "sopas-y-caldos" }, 
+  { :name => "ensaladas",             :slug => "ensaladas" }, 
+  { :name => "patatas",               :slug => "patatas" }, 
+  { :name => "huevos y lacteos",      :slug => "huevos-y-lacteos" }, 
+  { :name => "soja y derivados",      :slug => "soja-y-derivados" }, 
+  { :name => "frutas",                :slug => "frutas" },
+  { :name => "embutidos",             :slug => "embutidos" }, 
+  { :name => "bebidas",               :slug => "bebidas" }
+]
+
+Category.create(categories)
