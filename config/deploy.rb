80d2bf25b4786f95b9bf557a38147947f991f045
Gabriel Ortuño <arctarusnet@gmail.com>
2011-06-29 11:37:10 +0200

    require rake 0.9.2

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,10 +6,11 @@ $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 # Load RVM's capistrano plugin.
 require 'rvm/capistrano'
 # Set it to the ruby + gemset of your app, e.g:
-set :rvm_ruby_string, 'ruby-1.9.2-p180'
+set :rvm_ruby_string, '1.9.2@rezets'
+set :rvm_type, :user
 
 set :application, "rezets"
-set :repository,  "git://github.com/arctarus/rezets.git"
+set :repository,  "git@github.com:arctarus/rezets.git"
 set :db_name, 'rm'
 set :db_user, 'rezets'
 
@@ -20,20 +21,20 @@ set :use_sudo, false
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 set :branch, "stable"
-set :git_shallow_clone, 1
-set :ssh_options, { :forward_agent => true }
-set :deploy_via, :remote_cache
-set :deploy_to, "/home/arctarus/public_html/#{application}"
+# set :git_shallow_clone, 1
+# set :ssh_options, { :forward_agent => true }
+set :deploy_via, :copy
+set :deploy_to, "/home/#{user}/public_html/#{application}"
 
-default_run_options[:pty] = true
+# default_run_options[:pty] = true
 
 # role :web, "67.23.21.106"                          # Your HTTP server, Apache/etc
 # role :app, "67.23.21.106"                          # This may be the same as your `Web` server
 # role :db,  "67.23.21.106", :primary => true        # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
 
-server 'rezets.com', :app, :web
-role :db, 'rezets.com', :primary => true
+server '67.23.21.106', :app, :web
+role :db, '67.23.21.106', :primary => true
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
