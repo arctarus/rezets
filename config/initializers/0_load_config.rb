6b33486876e25c63c54b70d6f051b9738997a2be
Gabriel Ortuño <arctarusnet@gmail.com>
2011-08-04 21:08:50 +0200

    mailer config

--- config/initializers/0_load_config.rb
+++ config/initializers/0_load_config.rb
@@ -1,1 +1,17 @@
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+if Rails.env != 'production'
+  APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+else
+  APP_CONFIG = {
+    'app_domain'            => 'rezets.com',
+    'allow_outgoing_email'  => true,
+    'outgoing'              => {
+      'tls'   => false,
+      'host'  => 'smtp.sendgrid.net',
+      'port'  => '25',
+      'auth'  => :plain,
+      'from'  => ENV['SENDGRID_DOMAIN'],
+      'user'  => ENV['SENDGRID_USERNAME'],
+      'pass'  => ENV['SENDGRID_PASSWORD']
+    }
+  }
+end
