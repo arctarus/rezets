e1ff2bc7f7c9ff68d49671a0abc671f5ea8f464e
Gabriel Ortuño <arctarusnet@gmail.com>
2011-08-10 19:23:37 +0200

    added custom actions to ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,13 +4,18 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
 
-    can :read, User
+    can [:read, :following, :likes], User
     can :manage, User, :id => user.id
 
     can :read, Recipe
     can :manage, Recipe, :author_id => user.id
 
     can [:read, :create], Invitation, :sender_id => user.id
+
+    # logged
+    if not user.new_record?
+      can [:follow, :unfollow], User
+    end
 
     # admin
     if user.id == 1
