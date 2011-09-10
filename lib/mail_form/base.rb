8c96462ea4f8ed96aba3c90c7051e2496cd39031
Gabriel Ortuño <arctarusnet@gmail.com>
2011-09-10 11:47:06 +0200

    attributes present and active model compilance

--- lib/mail_form/base.rb
+++ lib/mail_form/base.rb
@@ -1,25 +1,42 @@
 module MailForm
   class Base
-    # 1) Include attribute methods behavior
     include ActiveModel::AttributeMethods
+    include ActiveModel::Conversion
+    include ActiveModel::Validations
+    extend ActiveModel::Naming
+    extend ActiveModel::Translation
 
-    # 2) Define "clear_" as an attribute prefix
+    class_attribute :_attributes
+    self._attributes = []
+
     attribute_method_prefix 'clear_'
+    attribute_method_suffix '?'
 
     def self.attributes(*names)
       attr_accessor *names
-
-      # 3) Ask to define the prefix methods for the given attributes names
       define_attribute_methods names
+      self._attributes += names
+    end
+
+    def attributes
+      self._attributes.inject({}) do |hash, attr|
+        hash[attr.to_s] = send(attr)
+        hash
+      end
+    end
+    
+    def persisted?
+      false
     end
 
     protected
 
-    # 4) Since we declared a "clear_" prefix, it expects to have a
-    # "clear_attribute" method defined, which receives an attribute
-    # name and implements the clearing logic.
     def clear_attribute(attribute)
       send("#{attribute}=", nil)
+    end
+
+    def attribute?(attribute)
+      send(attribute).present?
     end
 
   end
--- spec/models/feedback_mail_spec.rb
+++ spec/models/feedback_mail_spec.rb
@@ -32,7 +32,66 @@ describe FeedbackMail do
       feedback_mail.clear_email
       feedback_mail.email.should be_nil
     end
+  end
 
+  describe "ask if attribute is present" do
+    it "name" do
+      feedback_mail.name?.should be_false
+      feedback_mail.name = "User"
+      feedback_mail.name?.should be_true
+    end
+
+    it "email" do
+      feedback_mail.email?.should be_false
+      feedback_mail.email = "user@test.com"
+      feedback_mail.email?.should be_true
+    end
+  end
+
+  describe "compilance to active model" do
+    require 'test/unit/assertions'
+    require 'active_model/lint'
+    include Test::Unit::Assertions
+    include ActiveModel::Lint::Tests
+
+    # to_s is to support ruby-1.9
+    active_model_lint_tests = 
+      ActiveModel::Lint::Tests.public_instance_methods.map(&:to_s).grep(/^test/)
+
+    active_model_lint_tests.each do |m|
+      example m.gsub('_',' ') do
+        send m
+      end
+    end
+
+    def model
+      feedback_mail
+    end
+
+    it "expose singular name" do
+      model.class.model_name.singular.should == "feedback_mail"
+    end
+
+    it "expose human name" do
+      model.class.model_name.human.should == "Feedback mail"
+    end
+
+    it "use I18n" do
+      begin
+        I18n.backend.store_translations :es, 
+        :activemodel => { :models => { :feedback_mail => 'My Feedback Mail' } }
+        model.class.model_name.human.should == "My Feedback Mail"
+      ensure
+        I18n.reload!
+      end
+    end
+
+    it "retrieve all attributes values" do
+      feedback_mail.name = "User"
+      feedback_mail.email = "user@test.com"
+      feedback_mail.attributes["name"].should == "User"
+      feedback_mail.attributes["email"].should == "user@test.com"
+    end
   end
 
 end
