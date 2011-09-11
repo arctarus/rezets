b8bc73a64f7ad524f9164470afff21abdcd179b3
Gabriel Ortuño <arctarusnet@gmail.com>
2011-09-11 13:57:36 +0200

    add flag COVERAGE to start simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' if ENV['COVERAGE']
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
