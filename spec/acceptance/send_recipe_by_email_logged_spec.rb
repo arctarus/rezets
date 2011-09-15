b8186a7bef4a96cfd7987ad973939a572eb45994
Gabriel Ortuño <arctarusnet@gmail.com>
2011-09-15 19:52:56 +0200

    acceptance for send recipe by email

---
+++ spec/acceptance/send_recipe_by_email_logged_spec.rb
@@ -0,0 +1,30 @@
+require 'acceptance/acceptance_helper'
+
+feature 'Send recipe by email logged', %q{
+  In order to share recipes
+  As a logged user
+  I want send a recipe to my friends
+} do
+
+  background do
+    @author = sample :user
+    @recipe = sample :recipe, :author => @author
+    @author.recipes << @recipe
+    @user = sample :user
+    @email = sample_email
+    @message = sample_paragraphs
+    login_as @user
+  end
+
+  scenario 'loged user', :js => true do
+    visit user_recipe_path(@author, @recipe)
+    click_link _('email')
+    fill_in _('email'),     :with => @email
+    click_link _('add a message?')
+    fill_in _('message'),   :with => @message
+    click_button _('send')
+    wait_until { page.should have_content _('recipe sent') }
+    last_mail.to.should == [@email]
+  end
+
+end
---
+++ spec/acceptance/send_recipe_by_email_spec.rb
@@ -0,0 +1,38 @@
+require 'acceptance/acceptance_helper'
+
+feature 'Send a recipe by email', %q{
+  In order to share recipes
+  As a guest
+  I want send a recipe to my friends
+} do
+
+  background do
+    @author = sample :user
+    @recipe = sample :recipe, :author => @author
+    @author.recipes << @recipe
+    @email = sample_email
+    @message = sample_paragraphs
+  end
+
+  scenario 'guest user', :js => true do
+    visit user_recipe_path(@author, @recipe)
+    click_link _('email')
+    fill_in _('your name'), :with => sample_name
+    fill_in _('email'),     :with => @email
+    click_link _('add a message?')
+    fill_in _('message'),   :with => @message
+    click_button _('send')
+    wait_until { page.should have_content _('recipe sent') }
+    last_mail.to.should == [@email]
+  end
+
+  scenario 'guest user forget one field', :js => true do
+    visit user_recipe_path(@author, @recipe)
+    click_link _('email')
+    fill_in _('email'),     :with => @email
+    click_link _('add a message?')
+    fill_in _('message'),   :with => @message
+    click_button _('send')
+  end
+
+end
