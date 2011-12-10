68b9823429808b72ca10a1786e35a74170990545
Gabriel Ortuño <arctarusnet@gmail.com>
2011-12-10 12:06:46 +0100

    not load APP_CONFIG in production from file

--- config/initializers/0_load_config.rb
+++ config/initializers/0_load_config.rb
@@ -1,7 +1,17 @@
-APP_CONFIG = YAML.load_file(Rails.root.join("config/config.yml"))[Rails.env]
-
-APP_CONFIG['outgoing'].merge!({
-  'from' => ENV['SENDGRID_DOMAIN'],
-  'user' => ENV['SENDGRID_USERNAME'],
-  'pass' => ENV['SENDGRID_PASSWORD']
-}) if Rails.env == 'production'
+if Rails.env != 'production'
+  APP_CONFIG = YAML.load_file(Rails.root.join("config/config.yml"))[Rails.env]
+else
+  APP_CONFIG = {
+    'app_domain' => 'rezets.com',
+    'allow_outgoing_email' => true,
+    'outgoing' => {
+      'tls' => false,
+      'host' => 'smtp.sendgrid.net',
+      'port' => '25',
+      'auth' => :plain,
+      'from' => ENV['SENDGRID_DOMAIN'],
+      'user' => ENV['SENDGRID_USERNAME'],
+      'pass' => ENV['SENDGRID_PASSWORD']
+    }
+  })
+end
