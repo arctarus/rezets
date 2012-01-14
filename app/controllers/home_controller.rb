48698d8bed2ea6314ff0c82bb837a1a22d2a3cf5
Gabriel Ortuño <arctarusnet@gmail.com>
2012-01-14 20:03:24 +0100

    move query to model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   respond_to :html, :except => :feed
 
   def index
-    @recipes = Recipe.order("created_at desc").limit(8)
+    @recipes = Recipe.freshly_made
     @categories = Category.with_recipes.order("name asc")
     @users = User.featured.limit(8)
   end
