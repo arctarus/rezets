3256ff2b681d8fff599354c809cb5737ebaa31ea
Gabriel Ortuño <arctarusnet@gmail.com>
2012-01-14 14:01:52 +0100

    change categories query to avoid group

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,14 +4,13 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
 
-    can [:create, :read, :following, :likes], User
+    can [:create, :read, :rookies], User
     can [:read, :print, :email, :email_send], Recipe
     can :read, Category
     can :read, Ingredient
 
     # logged
     if not user.new_record?
-      can [:follow, :unfollow], User
       can :manage, User, id: user.id
       can :manage, Recipe, author_id: user.id
       can [:read, :create], Invitation, sender_id: user.id
