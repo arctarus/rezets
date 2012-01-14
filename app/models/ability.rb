fd86a9fe13915890eaeb249cbbedc2390e297257
Gabriel Ortuño <arctarusnet@gmail.com>
2012-01-14 20:03:43 +0100

    add load and authorize for index action

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,24 +2,28 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
+    @user = user || User.new # guest user (not logged in)
 
+    guest
+    logged if @user.persisted?
+    admin  if @user.id == 1
+  end
+
+  def guest
     can [:create, :read, :rookies], User
     can [:read, :print, :email, :email_send], Recipe
     can :read, Category
     can :read, Ingredient
+  end
 
-    # logged
-    if not user.new_record?
-      can :manage, User, id: user.id
-      can :manage, Recipe, author_id: user.id
-      can [:read, :create], Invitation, sender_id: user.id
-    end
+  def logged
+    can :manage, User, id: @user.id
+    can :manage, Recipe, author_id: @user.id
+    can [:read, :create], Invitation, sender_id: @user.id
+  end
 
-    # admin
-    if user.id == 1
-      can :manage, Invitation
-      can :manage, Ingredient
-    end
- end
+  def admin
+    can :manage, Invitation
+    can :manage, Ingredient
+  end
 end
